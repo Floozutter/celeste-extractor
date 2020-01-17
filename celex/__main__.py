2d4a386379f1fab508bf135f3500c61154d4b150
Floozutter <floozutter@gmail.com>
2020-01-16 16:19:07 -0800

    Successor successor successor successor successor commit

--- celex/__main__.py
+++ celex/__main__.py
@@ -1,11 +1,23 @@
-from PIL import Image
 from celex import decode
+from celex import fileio
+
+from PIL import Image
+from ntpath import basename
 
 
 def main():
-    with open("deadpan00.data", "rb") as ifile:
-        a = decode.decode(ifile.read())
-    a.save("a.png")
+    dirpath = input("Path to the Celeste graphics directory: ")
+    filepaths = fileio.celeste_datafiles(dirpath)
+    for path in filepaths:
+        print(path)
+        with open(path, "rb") as ifile:
+            try:
+                img = decode.decode(ifile.read())
+                img.save("./output/" + basename(path) + ".png")
+            except MemoryError:
+                print("\tOops, out of memory!")
+            except StopIteration:
+                print("\tOops, bad code!")
 
 
 if __name__ == "__main__":
